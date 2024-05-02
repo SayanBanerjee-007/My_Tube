d99fb42a6a50626d8da32c52e78b38a5b46f70d2
Sayan Banerjee <sayankb.001@gmail.com>
2024-05-02 13:07:56 +0530

    Refactor subscription controller to include new features

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,21 +1,142 @@
 import mongoose, { isValidObjectId } from 'mongoose'
-import { User } from '../models/user.model.js'
 import { Subscription } from '../models/subscription.model.js'
 import { asyncHandler, ApiError, ApiResponse } from '../utils/index.js'
 
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+  const channels = await Subscription.aggregate([
+    {
+      $match: {
+        subscriber: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'channel',
+        foreignField: '_id',
+        as: 'channel',
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              username: 1,
+              fullName: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $unwind: '$channel',
+    },
+  ])
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { channels },
+        'Subscribed channels list fetched successfully.'
+      )
+    )
+})
+
+const getSubscriberCount = asyncHandler(async (req, res) => {
+  const { channelId } = req.params
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, 'Invalid channel ID.')
+  }
+
+  const subscriberCount = await Subscription.countDocuments({
+    channel: channelId,
+  })
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { subscriberCount },
+        'Subscriber count fetched successfully.'
+      )
+    )
+})
+
 const toggleSubscription = asyncHandler(async (req, res) => {
   const { channelId } = req.params
-  // TODO: toggle subscription
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, 'Invalid channel ID.')
+  }
+
+  const subscription = await Subscription.findOne({
+    channel: channelId,
+    subscriber: req.user._id,
+  })
+  if (subscription) {
+    await Subscription.findByIdAndDelete(subscription._id)
+    res
+      .status(200)
+      .json(new ApiResponse(200, null, 'Unsubscribed successfully.'))
+  } else {
+    await Subscription.create({
+      channel: channelId,
+      subscriber: req.user._id,
+    })
+    res.status(200).json(new ApiResponse(200, null, 'Subscribed successfully.'))
+  }
 })
 
-// controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
   const { channelId } = req.params
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, 'Invalid channel ID.')
+  }
+  if (req.user._id.toString() !== channelId) {
+    throw new ApiError(403, 'You are not authorized to view this resource.')
+  }
+  const subscribers = await Subscription.aggregate([
+    {
+      $match: {
+        channel: new mongoose.Types.ObjectId(channelId),
+      },
+    },
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'subscriber',
+        foreignField: '_id',
+        as: 'subscriber',
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              username: 1,
+              fullName: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $unwind: '$subscriber',
+    },
+  ])
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { subscribers },
+        'Subscribers list fetched successfully.'
+      )
+    )
 })
 
-// controller to return channel list to which user has subscribed
-const getSubscribedChannels = asyncHandler(async (req, res) => {
-  const { subscriberId } = req.params
-})
-
-export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels }
+export {
+  toggleSubscription,
+  getUserChannelSubscribers,
+  getSubscribedChannels,
+  getSubscriberCount,
+}
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,5 +1,19 @@
 import { Router } from 'express'
+import {
+  getSubscribedChannels,
+  getSubscriberCount,
+  getUserChannelSubscribers,
+  toggleSubscription,
+} from '../controllers/subscription.controller.js'
+import { verifyJWT } from '../middlewares/auth.middleware.js'
 
 const subscriptionRouter = Router()
+
+subscriptionRouter.route('/').get(verifyJWT, getSubscribedChannels)
+subscriptionRouter
+  .route('/channel/:channelId')
+  .get(getSubscriberCount)
+  .post(verifyJWT, toggleSubscription)
+subscriptionRouter.route('/user/:subscriberId').get(getUserChannelSubscribers)
 
 export { subscriptionRouter }
